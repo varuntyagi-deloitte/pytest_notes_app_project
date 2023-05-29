e15bacaa8f095504d8a1ae449bbb298d801e1eab
Tyagi <varutyagi@deloitte.com>
2023-05-29 05:40:26 +0530

    The path of the log file to be created is changed

--- Logs/log_file.py
+++ Logs/log_file.py
@@ -8,7 +8,7 @@ class log_class:
         logger = logging.getLogger(loggerName)
         '''it will tell that right now this particular test file is being run 
         and the logs are printed in regarding to that test file'''
-        filename = logging.FileHandler('../Logs/logfile.log')
+        filename = logging.FileHandler('./Logs/logfile.log')
         # FileHandler method gives info. about the logging file where all the logs will be printed
         formatter = logging.Formatter("%(asctime)s : %(levelname)s : %(name)s : %(message)s")
         # Above the format is decided like in what format the log message is printed
